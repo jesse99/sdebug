d405d4528ebe104da0240929b79984dbdff22a00
Jesse Jones <jesse9jones@gmail.com>
2017-07-08 10:47:24 -0700

    More WIP on parsing input

--- src/input.rs
+++ src/input.rs
@@ -6,14 +6,12 @@ use std::env;
 
 type Handler = fn (&Vec<&str>) -> ();
 
-type Commands = Vec<(&'static str, Handler)>;
+type Commands = Vec<(Vec<&'static str>, Handler)>;
 
 pub fn read_lines()
 {
 	let hpath = get_history_path();
-	let verb_handlers: Commands = vec!(
-		("get", process_get),
-		("set", process_set));
+	let commands = init_commands();
 	
 	// TODO: instead of () install a completion fn
 	let mut editor = Editor::<()>::new();
@@ -30,8 +28,8 @@ pub fn read_lines()
 					},
 					// TODO: need to support help and ?
 					_ => {
-						editor.add_history_entry(&line);
-						process_line(&line, &verb_handlers);
+						editor.add_history_entry(&line);	// even if there is an error, it's nice to have it in the history so that the user can easily repair it
+						process_line(&line, &commands);
 					}
 				}
 			},
@@ -50,40 +48,104 @@ pub fn read_lines()
 	editor.save_history(&hpath).unwrap();
 }
 
-fn process_line(line: &str, handlers: &Commands)
+fn process_line(line: &str, commands: &Commands)
 {
-	let mut args: Vec<&str> = line.split_whitespace().collect();	// this is safe because score doesn't allow whitespace in component names
+	let args: Vec<&str> = line.split_whitespace().collect();	// this is safe because score doesn't allow whitespace in component names
 	if args.len() > 0 {
-		let matches = find_matching_handlers(args[0], handlers);
-		match matches.len() {
-			0 => print!("expected {}\n", handler_names(handlers).join(" or ")),	// TODO: use red?
-			1 => matches[0].1(&args.split_off(1)),
-			_ => print!("{} matches {}\n", args[0], handler_names(&matches).join(" and ")),
+		if let Some(handler) = find_handler(&args, commands) {
+			handler(&args);
+			
+		} else {
+			// We failed to find a command that matched what the user typed
+			// so check for some common error cases.
+			let mut matches = find_longer_commands(&args, commands);
+			if matches.is_empty() {
+				matches = find_shorter_commands(&args, commands);
+			}
+			if matches.len() > 0 {
+				println!("Did you mean:"); 	// TODO: use red?
+				for m in matches.iter() {
+					println!("   {}", m);
+				}
+			} else {
+				print!("failed to find a matching command\n");
+			}
 		}
 	}
 }
 
-fn handler_names(handlers: &Commands) -> Vec<&'static str>
+// Return a handler if args exactly matches a command.
+fn find_handler(args: &Vec<&str>, commands: &Commands) -> Option<Handler>
 {
-	handlers.iter().map(|h| h.0).collect()
+	let handlers: Vec<Handler> = commands.iter()
+		.filter(|command| args_matches(args, &command.0) && args.len() == command.0.len())
+		.map(|command| command.1)
+		.collect();
+	if handlers.len() == 1 {
+		Some(handlers[0])
+	} else {
+		None
+	}
 }
 
-fn find_matching_handlers(arg: &str, handlers: &Commands) -> Commands
+// Return the commands that args is a prefix of, e.g. "get" matches "get logs all", "get state", etc.
+fn find_longer_commands(args: &Vec<&str>, commands: &Commands) -> Vec<String>
 {
-	handlers.iter()
-		.filter(|h| h.0.starts_with(arg))
-		.map(|h| *h)
+	commands.iter()
+		.filter(|command| args_matches(args, &command.0) && args.len() < command.0.len())
+		.map(|command| command.0.join(" "))
 		.collect()
 }
 
+// Return the commands that are a prefix of args, e.g. "get log all hmm" matches "get log all".
+fn find_shorter_commands(args: &Vec<&str>, commands: &Commands) -> Vec<String>
+{
+	commands.iter()
+		.filter(|command| command_matches(args, &command.0) && args.len() > command.0.len())
+		.map(|command| command.0.join(" "))
+		.collect()
+}
+
+fn args_matches(args: &Vec<&str>, command: &Vec<&str>) -> bool
+{
+	args.iter()
+		.enumerate()
+		.all(|(i, _)| arg_matches(args, command, i))
+}
+
+fn command_matches(args: &Vec<&str>, command: &Vec<&str>) -> bool
+{
+	command.iter()
+		.enumerate()
+		.all(|(i, _)| arg_matches(args, command, i))
+}
+
+fn arg_matches(args: &Vec<&str>, command: &Vec<&str>, index: usize) -> bool
+{
+	if index < args.len() && index < command.len() {
+		args[index] == command[index]	// TODO: handle stuff like <secs>
+	} else {
+		false
+	}
+}
+
+fn init_commands() -> Commands
+{
+	vec!(
+		(vec!("get",    "log",  "all"),    get_log_all),
+		(vec!("goober", "log",  "all"),    get_log_all),
+		(vec!("set",    "time", "<secs>"), set_time_secs)
+	)
+}
+
 // get log all
-fn process_get(args: &Vec<&str>)
+fn get_log_all(args: &Vec<&str>)
 {
 	println!("get: {:?}", args);
 }
 
 // set time secs
-fn process_set(args: &Vec<&str>)
+fn set_time_secs(args: &Vec<&str>)
 {
 	println!("set: {:?}", args);
 }
