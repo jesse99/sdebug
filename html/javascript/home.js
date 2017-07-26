bc4d1f2e7f771135b5c3e662f079f78fc7a57416
Jesse Jones <jesse9jones@gmail.com>
2017-07-25 19:07:34 -0700

    Added Run Until button

--- html/javascript/home.js
+++ html/javascript/home.js
@@ -2,42 +2,73 @@
 
 var SDEBUG = {}
 SDEBUG.precision = 6;
+SDEBUG.exited = false;
 
 window.onload = function()
 {
-	set_precision();
-	refresh_header();
+	var run_until = document.getElementsByName("run-until")[0];
+	run_until.addEventListener("click", on_run_until);
+
+	start_set_precision();
+	start_refresh_header();
 };
 
-function set_precision()
+function on_run_until()
+{
+	var input = document.getElementById("run-time");
+
+	var request = new XMLHttpRequest();
+	request.addEventListener("load", finish_run_until);
+	request.addEventListener("error", on_ajax_error);
+	request.open("POST", "/time/" + input.value);
+	request.send();
+}
+
+function start_set_precision()
 {
 	var request = new XMLHttpRequest();
-	request.addEventListener("load", on_set_precision);
+	request.addEventListener("load", finish_set_precision);
 	request.addEventListener("error", on_ajax_error);
 	request.open("GET", "/time/precision");
 	request.send();
 }
 
-function refresh_header()
+function start_refresh_header()
 {
 	var request = new XMLHttpRequest();
-	request.addEventListener("load", on_refresh_header);
+	request.addEventListener("load", finish_refresh_header);
 	request.addEventListener("error", on_ajax_error);
 	request.open("GET", "/time");
 	request.send();
 }
 
-function on_set_precision(progress)
+function finish_run_until(progress)
+{
+	var json = JSON.parse(this.responseText);
+	if (json == "exited") {
+		SDEBUG.exited = true;
+	}
+	start_refresh_header();
+}
+
+function finish_set_precision(progress)
 {
 	SDEBUG.precision = JSON.parse(this.responseText);
 }
 
-function on_refresh_header(progress)
+function finish_refresh_header(progress)
 {
 	var json = JSON.parse(this.responseText);
 	var header = document.getElementById("header");
-	header.innerHTML = "Simulator @ " + json.toFixed(SDEBUG.precision) + "s";
-	console.log("time = ", json);
+	
+	var mesg = "Simulator @ " + json.toFixed(SDEBUG.precision) + "s";
+	if (SDEBUG.exited) {
+		mesg += " (exited)";
+	}
+	header.innerHTML = mesg;
+
+	var input = document.getElementById("run-time");
+	input.value = json;
 }
 
 function on_ajax_error(progress)
