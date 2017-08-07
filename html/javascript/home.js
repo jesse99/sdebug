f4f9338015a51c995e0bf10b007e9a7427b8337e
Jesse Jones <jesse9jones@gmail.com>
2017-08-06 19:38:07 -0700

    Better manage state. Started adding support for map view.

--- html/javascript/home.js
+++ html/javascript/home.js
@@ -1,24 +1,30 @@
 "use strict";
 
-/* global makeRequest, format:true */
+/* global SVG, makeRequest, format:true */
 
 var SDEBUG = {}
 SDEBUG.precision = 6;
 SDEBUG.last_logged_time = -1.0;
+SDEBUG.draw = null;
+
 SDEBUG.tab_name = "log";
+SDEBUG.get_current_state = get_current_log;			// () => promise
+SDEBUG.current_state_has_changed = log_has_changed;	// (current_state) => bool
+SDEBUG.apply_current_state = apply_log;				// (current_state) => void
 SDEBUG.old_state = {};
 
 window.onload = function()
 {
+	init_map();
+
 	var widget = document.getElementsByName("run-until")[0];
 	widget.addEventListener("click", () => {
 		var input = document.getElementById("run-time");
-		run_until("/time/" + input.value);
+		run_until(input.value);
 	});
 
-	/* TODO: When we support log filtering we'll need to re-submit the call if the change was filtered out. */
 	widget = document.getElementsByName("run-until-changed")[0];
-	widget.addEventListener("click", () => {run_until("/run/until/" + SDEBUG.tab_name + "-changed");});
+	widget.addEventListener("click", () => {run_until_changed();});
 
 	widget = document.getElementById("run-time");
 	widget.addEventListener("keyup", (event) => {
@@ -42,11 +48,12 @@ window.onload = function()
 	widget.addEventListener("click", () => {deselect_tabs("map", "log", "state"); select_tab("components");});
 
 	widget = document.getElementById("show-display-state");
-	widget.addEventListener("click", () => {SDEBUG.old_state = {}; refresh_states();});
+	widget.addEventListener("click", () => {SDEBUG.old_state = []; update_tab_view(null);});
 
 	deselect_tabs("map", "state", "components");
+	initialize_log();
 	get_precision();
-	sync_ui();
+	sync_ui(null);
 };
 
 function deselect_tabs(...names)
@@ -73,33 +80,65 @@ function select_tab(name)
 	tab.appendClass("is-active")
 
 	var view = document.getElementById(name + "-view");
-	if (name === "log" || name === "state") {
-		view.style.display = "table";
-	} else {
+	if (name == "components") {
+		console.error("need to set callbacks");
 		view.style.display = "inline";
-	}
 
-	if (name == "state") {
+	} else if (name == "log") {
+		SDEBUG.get_current_state = get_current_log;
+		SDEBUG.current_state_has_changed = log_has_changed;
+		SDEBUG.apply_current_state = apply_log;
+		initialize_log();
+		view.style.display = "table";
+
+	} else if (name == "map") {
+		console.error("need to set callbacks");
+		view.style.display = "inline";
+
+	} else if (name == "state") {
+		SDEBUG.get_current_state = get_current_state;
+		SDEBUG.current_state_has_changed = state_has_changed;
+		SDEBUG.apply_current_state = apply_state;
+		initialize_state();
+		view.style.display = "table";
+
 		view = document.getElementById("show-display-view");
 		view.style.display = "block";
+
+	} else {
+		console.error("bad name: " + name)
 	}
+
+	update_tab_view(null);
 }
 
-function sync_ui()
+function sync_ui(on_changed)
 {
 	refresh_exited();
 	refresh_header();
 	refresh_sub_header();
-
-	refresh_logs();
-	refresh_states();
+	return update_tab_view(on_changed);
 }
 
-function run_until(endpoint)
+function run_until(time)
 {
-	makeRequest("POST", endpoint)
+	makeRequest("POST", "/run/until/" + time)
 		.then(() => {			
-			sync_ui();
+			sync_ui(null);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+}
+
+function run_until_changed()
+{
+	makeRequest("POST", "/run/once")
+		.then((data) => {			
+			sync_ui((changed) => {
+				if (data != "exited" && !changed)
+					run_until_changed();
+			});
 		})
 		.catch((err) => {
 			console.error(err);
@@ -182,7 +221,47 @@ function refresh_sub_header()
 		});
 }
 
-function refresh_logs()
+function update_tab_view(on_changed)
+{
+	SDEBUG.get_current_state()
+		.then((new_state) => {			
+			const changed = SDEBUG.current_state_has_changed(new_state);
+			if (changed) {
+				SDEBUG.apply_current_state(new_state);
+				SDEBUG.old_state = new_state;
+			}
+			if (on_changed)
+				on_changed(changed);
+		})
+		.catch((err) => {
+			console.error(err);
+			if (on_changed)
+				on_changed(true);		// true so that we don't keep repeating something that isn't working
+		});	
+}
+
+// ---- Log -----------------------------------------------------------------------------
+// For logs old and new state will be [time, path, level, message].
+function initialize_log()
+{
+	SDEBUG.old_state = [];
+	SDEBUG.last_logged_time = -1.0;
+}
+
+function get_current_log()
+{
+	if (SDEBUG.last_logged_time < 0.0)
+		return makeRequest("GET", "/log");
+	else
+		return makeRequest("GET", "/log/after/" + SDEBUG.last_logged_time);
+}
+
+function log_has_changed(new_state)
+{
+	return new_state.length > 0;
+}
+
+function apply_log(new_state)
 {
 	function append_row(time, path, level, message)
 	{
@@ -202,34 +281,45 @@ function refresh_logs()
 		cell.appendClass("rightmost"); 
 	}
 
-	makeRequest("GET", "/log/after/" + SDEBUG.last_logged_time)
-		.then((data) => {	
-			for (var row of data) {		
-				append_row(row.time, row.path, row.level, row.message);
-				SDEBUG.last_logged_time = row.time;
-			}
-		})
-		.catch((err) => {
-			console.error(err);
-			append_row(SDEBUG.last_logged_time, "?", "Error", "AJAX failed");
-		});
-}
-
-function edit_value(path, kind, old)
-{
-	var value = prompt("New value: ", old);
-	if (value !== null) {
-		makeRequest("POST", format("/state/{0}/{1}/{2}", kind, path, value))
-			.then(() => {			
-				refresh_states();
-			})
-			.catch((err) => {
-				alert(format("POST failed: {0}", err));
-			});
+	var last = SDEBUG.last_logged_time;
+	for (var row of new_state) {
+		if (row.time > SDEBUG.last_logged_time) {	
+			append_row(row.time, row.path, row.level, row.message);
+			last = row.time;
+		}
 	}
+	SDEBUG.last_logged_time = last;
 }
 
-function refresh_states()
+// ---- State ---------------------------------------------------------------------------
+// For state old and new state will be [(path, value, kind)] where kind is "int", "float", or "string"
+// and the rows sorted by path.
+function initialize_state()
+{
+	SDEBUG.old_state = [];
+}
+
+function get_current_state()
+{
+	return makeRequest("GET", "/state/*");
+}
+
+function state_has_changed(new_state)
+{
+	if (new_state.length != SDEBUG.old_state.length)
+		return true;
+
+	for (var i = 0; i < new_state.length; i++) {
+		if (new_state[i][0] !== SDEBUG.old_state[i][0])
+			return true;
+		else if (new_state[i][1] !== SDEBUG.old_state[i][1])
+			return true;
+	}
+
+	return false;
+}
+
+function apply_state(new_state)
 {
 	function append_row(path, value, klass, kind)
 	{
@@ -259,51 +349,55 @@ function refresh_states()
 			cell.addEventListener("click", () => {edit_value(path, kind, value);});
 	}
 
-	makeRequest("GET", "/state/*")
-		.then((state) => {	
-			var body = document.getElementById("state-body");
-			while (body.rows.length > 0) {
-				body.deleteRow(-1);
-			}
+	var body = document.getElementById("state-body");
+	while (body.rows.length > 0) {
+		body.deleteRow(-1);
+	}
 
-			var new_state = {};
-			var updated = false;
-			var path = "";
-			for (var row of state) {
-				path = row[0];
-				var value = row[1];
-				var kind = row[2];
-				if (show_state(path)) {
-					var klass = "";
-					if (!(path in SDEBUG.old_state))
-						klass = "added";
-					else if (SDEBUG.old_state[path] !== value)
-						klass = "changed";
-					append_row(path, value, klass, kind);
-					new_state[path] = value;
+	var row = [];
+	var old_values = {};	// create some temporary maps so that we can do lookups
+	for (row of SDEBUG.old_state) {
+		old_values[row[0]] = row[1];
+	}
 
-					if (klass !== "")
-						updated = true;
-				}
-			}
+	var path = "";
+	var new_values = {};
+	for (row of new_state) {
+		path = row[0];
+		var value = row[1];
+		var kind = row[2];
+		if (show_state(path)) {
+			var klass = "";
+			if (!(path in old_values))
+				klass = "added";
+			else if (old_values[path] !== value)
+				klass = "changed";
+			append_row(path, value, klass, kind);
+		}
+		new_values[path] = value;
+	}
 
-			for (path in SDEBUG.old_state)
-			{
-				if (!(path in new_state)) {
-					append_row(path, SDEBUG.old_state[path], "removed");
-					updated = true;
-				}
-			}
+	for (path in old_values)
+	{
+		if (!(path in new_values)) {
+			if (show_state(path))
+				append_row(path, old_values[path], "removed");
+		}
+	}
+}
 
-			SDEBUG.old_state = new_state;
-
-			if (!updated && SDEBUG.tab_name == "state")
-				run_until("/run/until/state-changed");
-		})
-		.catch((err) => {
-			console.error(err);
-			append_row("?", "AJAX failed");
-		});
+function edit_value(path, kind, old)
+{
+	var value = prompt("New value: ", old);
+	if (value !== null) {
+		makeRequest("POST", format("/state/{0}/{1}/{2}", kind, path, value))
+			.then(() => {			
+				update_tab_view(null);
+			})
+			.catch((err) => {
+				alert(format("POST failed: {0}", err));
+			});
+	}
 }
 
 function show_state(path)
@@ -311,3 +405,31 @@ function show_state(path)
 	var checkbox = document.getElementById("show-display-state");
 	return checkbox.checked || !path.includes(".display-");
 }
+
+// ---- Map -----------------------------------------------------------------------------
+function init_map()	// TODO: this should be an initialize method
+{
+	makeRequest("GET", "/state/*.display-size-x")
+		.then((data) => {			
+			const width = data[0][1];
+			makeRequest("GET", "/state/*.display-size-y")
+				.then((data2) => {			
+					const height = data2[0][1];
+					SDEBUG.draw = SVG("map-view").size("100%", 1000);
+					SDEBUG.draw.viewbox(0, 0, width, height);
+					refresh_map()
+				});
+		})
+		.catch((err) => {
+			// TODO: disable the map tab?
+		});
+}
+
+function refresh_map()
+{
+	if (SDEBUG.draw) {
+		SDEBUG.draw.clear();
+		SDEBUG.draw.rect(25, 25).attr({"fill": "#f06"});
+	}
+}
+
